76aa68d99c401b548e72619dd2bf94f251e2ab4a
Nick Hsueh <nickhsueh@MPB-2019.local>
2021-11-27 15:34:51 +0800

    templates for showing prize

--- bonus/views.py
+++ bonus/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+from .models import Prize
 
 def index(request):
-    return HttpResponse("來試試運氣吧！")
+    prizeList = Prize.objects.all()
+
+    return render(
+        request,
+        'bonus/index.html',
+        context = {'prize_list': prizeList})
