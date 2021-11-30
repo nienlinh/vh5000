78140d9a14e7bbc80ee033cd34cf2a4c84394eb1
nlh 17 <nlhsueh@mail.fcu.edu.tw>
2021-11-30 08:38:39 +0800

    show all winners

--- bonus/views.py
+++ bonus/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-from .models import Prize
+from .models import Prize, Winner
+
 
 def index(request):
     prizeList = Prize.objects.all()
+    winnerList = Winner.objects.all()
 
     return render(
         request,
         'bonus/index.html',
-        context = {'prize_list': prizeList})
+        context={'prize_list': prizeList, 'winner_list': winnerList})
