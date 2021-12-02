e24ebe99b2dba4698d1b0859e22b9bcbf75adf4b
Nick Hsueh <nickhsueh@MPB-2019.local>
2021-12-02 13:50:11 +0800

    .

--- vh5000/settings.py
+++ vh5000/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+import os
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, '/bonus/static/')
+)
 
 
 # Default primary key field type
