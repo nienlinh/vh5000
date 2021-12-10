f4d525d1bb4b2b45761251f25155071b7340956b
Lai <m0830074@mail.fcu.edu.tw>
2021-12-10 13:36:46 +0800

    add javascript to adjust radio and add update function

--- register/urls.py
+++ register/urls.py
@@ -18,5 +18,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('update/<int:pk>', views.update, name='Update')
 ]
 
--- register/views.py
+++ register/views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Person, Voucher
 from .forms import PersonModelForm
 # Create your views here.
 
 def index(request):
     form = PersonModelForm()
+
+    if request.method == "POST":
+        form = PersonModelForm(request.POST)
+        if form.is_valid():
+            form_id = form.save()
+            message = {'choice': form.cleaned_data['voucher_id'],
+                       'id': form_id.id}
+            print(form.cleaned_data['voucher_id'])
+        return render(request, "register/result.html", message)
+
     context = {
         'form': form
     }
 
     return render(request, "register/index.html", context)
+
+def update(request, pk):
+    person = Person.objects.get(id=pk)
+    form = PersonModelForm(instance=person)
+
+    if request.method == "POST":
+        form = PersonModelForm(request.POST, instance=person)
+        if form.is_valid():
+            form_id = form.save()
+            message = {'choice': form.cleaned_data['voucher_id'],
+                       'id': form_id.id}
+            print(form.cleaned_data['voucher_id'])
+        return render(request, "register/result.html", message)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "register/update.html", context)
