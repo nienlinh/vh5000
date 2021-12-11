820503db01871ab387195ad52d9261cd316f44eb
Nick Hsueh <nickhsueh@MPB-2019.local>
2021-12-12 00:14:02 +0800

    valid check

--- register/forms.py
+++ register/forms.py
@@ -6,10 +6,39 @@ class PersonModelForm(forms.ModelForm):
 
     class Meta:
         model = Person
-        fields = '__all__'  # __all__ 表示要全部呈現中所有欄位
+        fields = ['ssn', 'tel', 'voucher_id'] 
+
+        # widget 用來設定介面的呈現
+        widgets = {
+            'ssn': forms.TextInput(attrs = {'class': 'form-control'}),
+            'tel': forms.TextInput(attrs = {'class': 'form-control'}),
+            'voucher_id': forms.RadioSelect()
+            # 'voucher_id': forms.Select(attrs = {'class': 'form-control'}),
+        }
 
         labels = {
             'ssn': '身分證字號',
             'tel': '電話號碼',
             'voucher_id': '綁定'
         }
+
+    # override the clean_<fieldname>() method
+    # 身分證必須剛好 10 個字
+    def clean_ssn(self):
+        data = self.cleaned_data['ssn'] # 通過基本檢查乾淨資料
+
+        if len(data) != 10 :
+            raise ValidationError('身分證長度必須為 10')
+
+        # Remember to always return the cleaned data.
+        return data
+
+    # 電話號碼必須大於五個字
+    def clean_tel(self):
+        data = self.cleaned_data['tel']
+
+        if len(data) < 5 :
+            raise ValidationError('電話長度不可小於5')
+
+        # Remember to always return the cleaned data.
+        return data        
--- register/views.py
+++ register/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from .models import Person
 from .forms import PersonModelForm
-from django.http import HttpResponse
 
 def index(request):
 
     if request.method == "POST":
         # save to DB
         form = PersonModelForm(request.POST)
+        context = {'form': form}
         if form.is_valid():
             form.save()
             # re-direct to a html (show success information)
-            context = {'form': form}
-            return render(request, "register/apply_success.html", context)            
+            return render(request, "register/apply_success.html", context) 
+        else:
+            return render(request, "register/apply_fail.html", context) 
     
     form = PersonModelForm()
 
